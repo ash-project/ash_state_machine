3b57f4e61526950eeb34fa33238dfb29e85c4a2d
Zach Daniel <zach@zachdaniel.dev>
2023-04-22 19:42:54 -0600

    chore: fix git_ops config

--- config/config.exs
+++ config/config.exs
@@ -18,7 +18,7 @@ if Mix.env() == :dev do
     # Pass in `true` to use `"README.md"` or a string to customize
     manage_readme_version: [
       "README.md",
-      "documentation/tutorials/getting-started-with-state-machines.md"
+      "documentation/tutorials/get-started-with-state-machines.md"
     ],
     version_tag_prefix: "v"
 end
