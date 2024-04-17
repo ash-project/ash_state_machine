7f80527f5190d95624b18130291aab6d00422423
Zach Daniel <zach@zachdaniel.dev>
2024-04-17 01:58:33 +0100

    improvement: add atomic implementation
    closes #16

--- lib/builtin_changes/transition_state.ex
+++ lib/builtin_changes/transition_state.ex
@@ -5,4 +5,53 @@ defmodule AshStateMachine.BuiltinChanges.TransitionState do
   def change(changeset, opts, _) do
     AshStateMachine.transition_state(changeset, opts[:target])
   end
+
+  def atomic(changeset, opts, _) do
+    transitions =
+      AshStateMachine.Info.state_machine_transitions(changeset.resource, changeset.action.name)
+
+    attribute = AshStateMachine.Info.state_machine_state_attribute!(changeset.resource)
+    old_state = expr(^ref(attribute))
+    target = opts[:target]
+
+    all_states = AshStateMachine.Info.state_machine_all_states(changeset.resource)
+
+    states_expr =
+      Enum.reduce(transitions, nil, fn transition, expr ->
+        state_expr =
+          expr(^old_state in ^List.wrap(transition.from) and ^target in ^List.wrap(transition.to))
+
+        if is_nil(expr) do
+          state_expr
+        else
+          expr(state_expr or ^expr)
+        end
+      end)
+
+    new_state_value =
+      expr(
+        cond do
+          ^target not in ^all_states ->
+            error(
+              AshStateMachine.Errors.NoMatchingTransition,
+              %{old_state: ^old_state, target: ^target, action: ^changeset.action.name}
+            )
+
+          ^states_expr ->
+            ^opts[:target]
+
+          true ->
+            error(
+              AshStateMachine.Errors.NoMatchingTransition,
+              %{
+                old_state: ^old_state,
+                target: ^target,
+                action: ^changeset.action.name
+              }
+            )
+        end
+      )
+
+    {:atomic, %{attribute => new_state_value}}
+  end
 end
