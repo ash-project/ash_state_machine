7a6dc49ae597d59fba3a92d6e5b4e22148936f16
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-04-02 21:35:56 +0100

    clean up before?/after? callbacks

--- lib/transformers/add_state.ex
+++ lib/transformers/add_state.ex
@@ -5,12 +5,10 @@ defmodule AshStateMachine.Transformers.AddState do
   alias Spark.Dsl.Transformer
 
   def before?(Ash.Resource.Transformers.DefaultAccept), do: true
-  def before?(AshStateMachine.Transformers.SetDefaultInitialState), do: false
   def before?(_), do: false
 
-  def after?(Ash.Resource.Transformers.DefaultAccept), do: false
   def after?(AshStateMachine.Transformers.SetDefaultInitialState), do: true
-  def after?(_), do: true
+  def after?(_), do: false
 
   def transform(dsl_state) do
     deprecated_states = AshStateMachine.Info.state_machine_deprecated_states!(dsl_state)
