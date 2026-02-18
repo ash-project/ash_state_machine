aca4cc4aa4ec32ebdb555ea5174ad02550c7e0a9
Leon Qadirie <39130739+leonqadirie@users.noreply.github.com>
2026-02-19 00:38:57 +0100

    chore: SPDX contributor links (#142)

--- lib/ash_state_machine.ex
+++ lib/ash_state_machine.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2023 ash_state_machine contributors <https://github.com/ash-project/ash_state_machine/graphs.contributors>
+# SPDX-FileCopyrightText: 2023 ash_state_machine contributors <https://github.com/ash-project/ash_state_machine/graphs/contributors>
 #
 # SPDX-License-Identifier: MIT
 
